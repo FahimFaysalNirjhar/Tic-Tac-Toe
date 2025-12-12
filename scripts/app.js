24b947522e771108559a028bd6a8f5689944ab54
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2025-12-12 20:00:15 +0600

    tic tac toe game complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,13 @@
 let boxes = document.querySelectorAll(".box");
-let resebtn = document.querySelector("#reset-btn");
+let resetBtn = document.querySelector("#reset-btn");
+let main = document.querySelector("main");
+let msgContainer = document.querySelector("#msg-container");
+let msg = document.querySelector("#msg");
+let newBtn = document.querySelector("#New-btn");
 let turnX = true;
+
+const audio = new Audio("../assets/click.mp3");
+let successSound = new Audio("../assets/victory.mp3");
 
 const winPattens = [
   [0, 1, 2],
@@ -18,13 +25,27 @@ const disabledBoxes = () => {
     box.disabled = true;
   }
 };
+
+const enableBoxes = () => {
+  for (box of boxes) {
+    turnX = true;
+    box.disabled = false;
+    box.innerText = "";
+    audio.play();
+  }
+  main.classList.remove("hidden");
+  msgContainer.classList.add("hidden");
+};
+
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (turnX) {
+      audio.play();
       box.innerText = "X";
       box.style.color = "#c44536";
       turnX = false;
     } else {
+      audio.play();
       box.innerText = "O";
       box.style.color = "#772e25";
       turnX = true;
@@ -44,11 +65,19 @@ const checkWinner = () => {
     if (position1 != "" && position2 != "" && position3 != "") {
       if (position1 === position2 && position2 === position3) {
         console.log("winner");
-        disabledBoxes();
         showWinner(position1);
       }
     }
   }
 };
 
-const showWinner = (winner) => {};
+const showWinner = (winner) => {
+  main.classList.add("hidden");
+  msgContainer.classList.remove("hidden");
+  msg.innerText = `Congratulations! The winner is ${winner}.`;
+  disabledBoxes();
+  successSound.play();
+};
+
+newBtn.addEventListener("click", enableBoxes);
+resetBtn.addEventListener("click", enableBoxes);
